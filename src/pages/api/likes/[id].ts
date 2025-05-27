fdf284eff74e929d3b6cc3239d508b0a5c3dd204
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-05-27 15:37:08 -0400

    Construcción del endpoint PUT

--- src/pages/api/likes/[id].ts
+++ src/pages/api/likes/[id].ts
@@ -11,15 +11,14 @@ export const GET: APIRoute = async ({ params, request }) => {
 	const posts = await db.select().from(Posts).where(eq(Posts.id, postId));
 
 	if (posts.length === 0) {
-
-        const post = {
-            id: postId,
-            title: "Post not found",
-            likes: 0,
-        }
+		const post = {
+			id: postId,
+			title: "Post not found",
+			likes: 0,
+		};
 
 		return new Response(JSON.stringify(post), {
-			status: 200, // debería ser 404 pero por algo que el profe explicará más adelante se deja así, yo creo que como lo cita un botón de nuestra web, sí falla con un 404 algo crashea,en cambio con un 200 funciona y coloca 0 
+			status: 200, // debería ser 404 pero por algo que el profe explicará más adelante se deja así, yo creo que como lo cita un botón de nuestra web, sí falla con un 404 algo crashea,en cambio con un 200 funciona y coloca 0
 			headers: {
 				"Content-Type": "application/json",
 			},
@@ -32,4 +31,30 @@ export const GET: APIRoute = async ({ params, request }) => {
 			"Content-Type": "application/json",
 		},
 	});
+};
+
+export const PUT: APIRoute = async ({ params, request }) => {
+
+	const postId = params.id ?? "";
+
+	const posts = await db.select().from(Posts).where(eq(Posts.id, postId));
+	const { likes = 0 } = await request.json();
+
+	if (posts.length === 0) {
+		const newPost = {
+			id: postId,
+			title: "Post not found",
+			likes: 0,
+		};
+
+		await db.insert(Posts).values(newPost); // insertamos el nuevo post con 0 likes a la base de datos
+		posts.push(newPost); // agregamos el nuevo post al array de posts para que no falle el at(0) más adelante
+	}
+
+	const post = posts.at(0)!; // siempre tendremos un post 0, ya sea el que acabamos de crear o el que ya existía en la base de datos
+	post.likes = post.likes + likes;
+
+	await db.update(Posts).set(post).where(eq(Posts.id, postId));
+
+	return new Response(JSON.stringify("ok!"), {status: 200,});
 };
