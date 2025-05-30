7a91aa1dc0aa1dc62d6a31abb903f07035d318f7
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-05-30 18:11:47 -0400

    se obtiene la id del post del blog, se le pasa al btn react, con eso obtiene el número de likes y los muestra en pantalla

--- src/components/likes/BtnReact.jsx
+++ src/components/likes/BtnReact.jsx
@@ -1,30 +1,50 @@
+import { useState, useEffect } from "react";
 
-const BtnReact = () => {
-    const darLike = () => {
-        console.log("Like button clicked!");
-    }
+const BtnReact = ({ postId }) => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [likes, setLikes] = useState(0);
 
-    return (
-        <button
-            onClick={() => darLike()}
-            style={{
-                backgroundColor:"rgb(6, 9, 172)",
-                color: 'white',
-                padding: '10px 20px',
-                border: 'none',
-                borderRadius: '4px',
-                cursor: 'pointer',
-                transition: 'all 0.3s',
-                // background hover
-                ':hover': {
-                    backgroundColor: 'rgb(4, 6, 120)',
-                    transform: 'scale(1.05)'
-                }
-            }}
-        >
-            Like Counter
-        </button>
-    )
+	// obtener likes
+	useEffect(() => {
+		const fetchLikes = async () => {
+			try {
+				const response = await fetch(`/api/likes/${postId}`);
+				const data = await response.json();
+				setLikes(data.likes);
+			} catch (error) {
+				console.error("Error fetching likes:", error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchLikes();
+	}, [setLikes, setIsLoading, likes]);
+
+	const darLike = () => {
+		console.log("Like button clicked!");
+	};
+
+	return (
+		<button
+			onClick={() => darLike()}
+			style={{
+				backgroundColor: "rgb(6, 9, 172)",
+				color: "white",
+				padding: "10px 20px",
+				border: "none",
+				borderRadius: "4px",
+				cursor: "pointer",
+				transition: "all 0.3s",
+				// background hover
+				":hover": {
+					backgroundColor: "rgb(4, 6, 120)",
+					transform: "scale(1.05)",
+				},
+			}}
+		>
+			{isLoading ? "Loading..." : likes === 0 ? "Like me!" : `${likes} Likes`}
+		</button>
+	);
 };
 
 export default BtnReact;
